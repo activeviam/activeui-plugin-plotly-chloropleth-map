8f6e166d079436c8b521ed8da2bbbb2f77129671
Nouzbe <benjamin.amelot@gmail.com>
2021-02-28 17:12:34 +0100

    Fix widget icon tooltip

--- src/training/pluginChloroplethMap.tsx
+++ src/training/pluginChloroplethMap.tsx
@@ -13,4 +13,9 @@ export const pluginChloroplethMap: WidgetPlugin<ChloroplethMapState> = {
     widgetKey,
   },
   key: widgetKey,
+  translations: {
+    "en-US": {
+      key: "Chloropleth map",
+    },
+  },
 };
